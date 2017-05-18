f86f39d3664d037cc953a8ae3846079979826f93
Sreejith <sreejith.net@gmail.com>
2017-05-18 05:51:21 -0700

    Youtube search initial skeleton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
+const APT_KEY = 'AIzaSyDXOkXAF9-PGCt34IV4UfaVZAPfWoDlQOo';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
